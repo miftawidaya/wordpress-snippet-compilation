a144c2b9fbb65169401717afc51c13c8e664f451
miftawidaya <85409437+miftawidaya@users.noreply.github.com>
2023-03-26 11:35:00 +0700

    Update body-vardump.php

--- src/body-vardump.php
+++ src/body-vardump.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Theme functions and definitions
+ * Easy var_dump in wordpress
  *
  * @package wp-spppets
  */
@@ -8,16 +8,14 @@
 add_action( 'wp_body_open', 'body_open_print' );
 function body_open_print() {
 	
-	$cars = array (
-		array("Volvo",22,18),
-		array("BMW",15,13),
-		array("Saab",5,2),
-		array("Land Rover",17,15)
-	);
+	//put your data here
+	$var = array ( array("Volvo",22,18), array("BMW",15,13), array("Saab",5,2), array("Land Rover",17,15));
+	
+	$dump = $var;
 
 	echo '<div style="display:block; width:100%; padding: 36px; z-index: 999;">mantap</div>';
 	
-	highlight_string("<?php\n" . var_export($data, true) . ";\n?>");
+	highlight_string("<?php\n" . var_export($dump, true) . ";\n?>");
 	
 	echo '</div>';
 }
